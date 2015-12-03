5007f6013dc48328ed8b51899eb0e71881c68c0d
jocelyn brayet <jocelyn.brayet@curie.fr>
2015-12-03 14:18:54 +0100

    fix docker installation in VM 2

--- IFB_VM/install_ncPRO_docker.sh
+++ IFB_VM/install_ncPRO_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-mkdir -p /root/bowtie_indexes
-mkdir -p /root/annotations
-mkdir -p /root/data
-mkdir -p /root/results
+mkdir -p ~/bowtie_indexes
+mkdir -p ~/annotations
+mkdir -p ~/data
+mkdir -p ~/results
 
 docker pull institutcuriengsintegration/ncproseq:1.6.3
 
-echo alias ncproseq=\"docker run -i -t -v /root/bowtie_indexes/:/usr/curie_ngs/bowtie_indexes/ -v /root/annotations/:/usr/curie_ngs/annotation/ -v /root/data/:/usr/curie_ngs/rawdata/ -v /root/results/:/usr/curie_ngs/results_host/ institutcuriengsintegration/ncproseq:1.6.3\" >> /root/.bashrc
+echo alias ncproseq=\"docker run -i -t -v ~/bowtie_indexes/:/usr/curie_ngs/bowtie_indexes/ -v ~/annotations/:/usr/curie_ngs/annotation/ -v ~/data/:/usr/curie_ngs/rawdata/ -v ~/results/:/usr/curie_ngs/results_host/ institutcuriengsintegration/ncproseq:1.6.3\" >> ~/.bashrc
 
-. /root/.bashrc
+. ~/.bashrc
 
