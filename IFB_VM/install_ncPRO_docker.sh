2bc8e78cfa31cf8800daeae6f39cd76d5a5335f5
jocelyn brayet <jocelyn.brayet@curie.fr>
2015-12-03 11:25:52 +0100

    bashrc path

--- IFB_VM/install_ncPRO_docker.sh
+++ IFB_VM/install_ncPRO_docker.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-mkdir ~/bowtie_indexes
-mkdir ~/annotations
-mkdir ~/data
-mkdir ~/results
+mkdir /root/bowtie_indexes
+mkdir /root/annotations
+mkdir /root/data
+mkdir /root/results
 
 docker pull institutcuriengsintegration/ncproseq:1.6.3
 
-echo "alias ncproseq='docker run -i -t -v ~/bowtie_indexes/:/usr/curie_ngs/bowtie_indexes/ -v ~/annotations/:/usr/curie_ngs/annotation/ -v ~/data/:/usr/curie_ngs/rawdata/ -v ~/results/:/usr/curie_ngs/results_host/ institutcuriengsintegration/ncproseq:1.6.3" >> ~/.bashrc'"
+echo "alias ncproseq='docker run -i -t -v /root/bowtie_indexes/:/usr/curie_ngs/bowtie_indexes/ -v /root/annotations/:/usr/curie_ngs/annotation/ -v /root/data/:/usr/curie_ngs/rawdata/ -v /root/results/:/usr/curie_ngs/results_host/ institutcuriengsintegration/ncproseq:1.6.3' >> /root/.bashrc"
 
-source .bashrc
+source /root/.bashrc
