219eaebaf1c3747fd21e47310ac0dfd6167980f6
jocelyn brayet <jocelyn.brayet@curie.fr>
2015-12-03 13:38:15 +0100

    fix alias in docker installation

--- IFB_VM/install_ncPRO_docker.sh
+++ IFB_VM/install_ncPRO_docker.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-mkdir /root/bowtie_indexes
-mkdir /root/annotations
-mkdir /root/data
-mkdir /root/results
+mkdir -p /root/bowtie_indexes
+mkdir -p /root/annotations
+mkdir -p /root/data
+mkdir -p /root/results
 
 docker pull institutcuriengsintegration/ncproseq:1.6.3
 
-echo "alias ncproseq='docker run -i -t -v /root/bowtie_indexes/:/usr/curie_ngs/bowtie_indexes/ -v /root/annotations/:/usr/curie_ngs/annotation/ -v /root/data/:/usr/curie_ngs/rawdata/ -v /root/results/:/usr/curie_ngs/results_host/ institutcuriengsintegration/ncproseq:1.6.3' >> /root/.bashrc"
+echo alias ncproseq="docker run -i -t -v /root/bowtie_indexes/:/usr/curie_ngs/bowtie_indexes/ -v /root/annotations/:/usr/curie_ngs/annotation/ -v /root/data/:/usr/curie_ngs/rawdata/ -v /root/results/:/usr/curie_ngs/results_host/ institutcuriengsintegration/ncproseq:1.6.3" >> /root/.bashrc"
 
-source /root/.bashrc
+echo source /root/.bashrc
+
